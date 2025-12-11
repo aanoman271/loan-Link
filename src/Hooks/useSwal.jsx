5a56d0a9a46d39f16d165dddbc79f6481e9c3986
noman291 <aanoman291@gmail.com>
2025-12-11 19:44:36 +0600

    sweet alert in hook

---
+++ src/Hooks/useSwal.jsx
@@ -0,0 +1,34 @@
+import Swal from "sweetalert2";
+
+const useSwal = () => {
+  const success = (title = "Success!", text = "") => {
+    Swal.fire({
+      icon: "success",
+      title,
+      text,
+      timer: 2000,
+      showConfirmButton: false,
+    });
+  };
+
+  const err = (title = "Error!", text = "") => {
+    Swal.fire({ icon: "error", title, text });
+  };
+
+  const confirm = (title = "Are you sure?", text = "") => {
+    return Swal.fire({
+      title,
+      text,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes",
+      cancelButtonText: "Cancel",
+    });
+  };
+
+  return { success, err, confirm };
+};
+
+export default useSwal;
